bd8c91c1dab5b485dc5c5e70f12caf5ef1320ef9
favy12 <105597784+favy12@users.noreply.github.com>
2024-05-21 23:45:43 +0100

    Revert "D ev test"

--- nav.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.navMenu');
-
-hamburger.addEventListener('click', ()=>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
